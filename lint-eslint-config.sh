b6d54583bf1407baebf880ee0339004d25e7c5e7
Anatolii Litvinov <anatolii.litvinov.r@gmail.com>
2024-12-12 20:11:26 +0200

    chore: extend extension list of lintable files

--- lint-eslint-config.sh
+++ lint-eslint-config.sh
@@ -1,5 +1,5 @@
 cp eslint.config.js eslint.config.js.bak
 cp eslint.config.js __to_copy__eslint.config.js
-pnpm eslint __to_copy__eslint.config.js
+pnpm eslint __to_copy__eslint.config.js --fix
 rm eslint.config.js
 mv __to_copy__eslint.config.js eslint.config.js
