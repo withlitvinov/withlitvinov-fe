feab6e5ce3c78ffa8639dfee0da4459ab25d8a2c
Anatolii Litvinov <anatolii.litvinov.r@gmail.com>
2024-12-12 16:51:06 +0200

    chore: upgrade `tailwindcss` to v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
 declare module '@remix-run/node' {
   interface Future {
@@ -20,5 +21,13 @@ export default defineConfig({
       },
     }),
     tsconfigPaths(),
+    tailwindcss(),
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
 });
